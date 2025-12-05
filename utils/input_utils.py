2758dde665c41389cf8dcf89dc2d70a1f5047fa4
Abel-Carpentier <abel.carpentier64@gmail.com>
2025-12-05 12:33:08 +0100

    Premier truc

--- utils/input_utils.py
+++ utils/input_utils.py
@@ -1,1 +1,7 @@
 def demander_texte(message):
+    while True:
+        texte = input(message).strip()
+        if texte:
+            return texte
+        else:
+            print("Invalide, veuillez entrer un texte non vide.")

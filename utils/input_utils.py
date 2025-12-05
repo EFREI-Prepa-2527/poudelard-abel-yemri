7305680bf99b3a21338d3f5a1c3eaad1dafdc554
Abel-Carpentier <abel.carpentier64@gmail.com>
2025-12-06 00:37:49 +0100

    jad : add .gitignore

--- utils/input_utils.py
+++ utils/input_utils.py
@@ -6,6 +6,46 @@ def demander_texte(message):
         else:
             print("Invalide, veuillez entrer un texte non vide.")
 
+def demander_nombre(message, min_val=None, max_val=None):
+    while True:
+        texte = input(message)
+
+        if len(texte) == 0:
+            print("Veuillez entrer un nombre.")
+            continue
+
+        negatif = False
+        if texte[0] == '-':
+            negatif = True
+            texte_sans_signe = texte[1:]
+        else:
+            texte_sans_signe = texte
+
+        est_entier = True
+        for c in texte_sans_signe:
+            if c < '0' or c > '9':
+                est_entier = False
+                break
+
+        if not est_entier or texte_sans_signe == "":
+            print("Veuillez rentrer un nombre entier valide")
+            continue
+
+        nombre = 0
+        for c in texte_sans_signe:
+            nombre = nombre * 10 + (ord(c) - ord('0'))
+
+        if negatif:
+            nombre = -nombre
+
+        if (min_val is not None and min_val > nombre) or (max_val is not None and max_val < nombre):
+            print("Veuillez rentrer un nombre compris entre ", min_val, " et ", max_val, ".")
+            continue
+
+        return nombre
+
+
+            
 
 
 
